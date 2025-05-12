7368c69ecff2441dd878aa1949080fcabb5bac1c
Bernardo Vivian Vieira <90099589+bernardovvieira@users.noreply.github.com>
2025-05-12 20:06:38 -0300

    feat: adicionar dois agentes e quatro doenças monitoradas ao seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Doenca;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,16 +13,107 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
+        // Gestor
         User::factory()->create([
-            'use_nome' => 'Teste',
-            'use_cpf' => '111.111.111-11',
-            'use_email' => 'teste@exemplo.com',
+            'use_nome' => 'Gestor Teste',
+            'use_cpf' => '444.444.444-44',
+            'use_email' => 'gestor@exemplo.com',
             'use_senha' => bcrypt('senha123'),
             'use_perfil' => 'gestor',
             'use_aprovado' => true,
             'use_data_criacao' => now(),
         ]);
+
+        // Agente 1
+        User::factory()->create([
+            'use_nome' => 'Agente Um',
+            'use_cpf' => '111.111.111-11',
+            'use_email' => 'agente1@exemplo.com',
+            'use_senha' => bcrypt('senha123'),
+            'use_perfil' => 'agente',
+            'use_aprovado' => true,
+            'use_data_criacao' => now(),
+        ]);
+
+        // Agente 2
+        User::factory()->create([
+            'use_nome' => 'Agente Dois',
+            'use_cpf' => '222.222.222-22',
+            'use_email' => 'agente2@exemplo.com',
+            'use_senha' => bcrypt('senha123'),
+            'use_perfil' => 'agente',
+            'use_aprovado' => true,
+            'use_data_criacao' => now(),
+        ]);
+
+        // Agente 3
+        User::factory()->create([
+            'use_nome' => 'Agente Três',
+            'use_cpf' => '333.333.333-33',
+            'use_email' => 'agente3@exemplo.com',
+            'use_senha' => bcrypt('senha123'),
+            'use_perfil' => 'agente',
+            'use_aprovado' => false,
+            'use_data_criacao' => now(),
+        ]);
+
+        // Doenças monitoradas
+        $doencas = [
+            [
+                'doe_nome' => 'COVID-19',
+                'doe_sintomas' => [
+                    'Febre', 'Tosse seca', 'Fadiga', 'Dispneia', 'Anosmia', 'Ageusia',
+                    'Congestão nasal', 'Dor de cabeça', 'Dor abdominal', 'Diarreia'
+                ],
+                'doe_transmissao' => [
+                    'Gotículas respiratórias', 'Aerossois (transmissão aérea)', 'Fômites'
+                ],
+                'doe_medidas_controle' => [
+                    'Higienização das mãos', 'Máscara cirúrgica', 'Máscara N95/FFP2',
+                    'Ventilação adequada', 'Isolamento de casos', 'Vacinação'
+                ],
+            ],
+            [
+                'doe_nome' => 'Dengue',
+                'doe_sintomas' => [
+                    'Febre', 'Dor de cabeça', 'Mialgias', 'Artralgias', 'Rash cutâneo', 'Náusea'
+                ],
+                'doe_transmissao' => [
+                    'Vetor biológico'
+                ],
+                'doe_medidas_controle' => [
+                    'Controle de vetores', 'Educação em saúde', 'Vigilância epidemiológica'
+                ],
+            ],
+            [
+                'doe_nome' => 'Hepatite A',
+                'doe_sintomas' => [
+                    'Febre', 'Náusea', 'Vômito', 'Dor abdominal', 'Diarreia'
+                ],
+                'doe_transmissao' => [
+                    'Água contaminada', 'Alimentos contaminados', 'Transmissão fecal-oral'
+                ],
+                'doe_medidas_controle' => [
+                    'Higienização das mãos', 'WASH (água, saneamento e higiene)', 'Vacinação'
+                ],
+            ],
+            [
+                'doe_nome' => 'Influenza',
+                'doe_sintomas' => [
+                    'Febre', 'Tosse', 'Calafrios', 'Fadiga', 'Congestão nasal', 'Dor de cabeça'
+                ],
+                'doe_transmissao' => [
+                    'Gotículas respiratórias', 'Fômites', 'Contato direto'
+                ],
+                'doe_medidas_controle' => [
+                    'Máscara cirúrgica', 'Etiqueta respiratória', 'Vacinação',
+                    'Distanciamento físico', 'Ventilação adequada'
+                ],
+            ],
+        ];
+
+        foreach ($doencas as $doenca) {
+            Doenca::create($doenca);
+        }
     }
 }
