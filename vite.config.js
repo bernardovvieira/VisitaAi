f523659c3ac7f7b95db1e54badf46c0e27c11fc0
Bernardo Vivian Vieira <bernardo@bitwise.dev.br>
2026-02-09 13:08:49 -0300

    chore: corrigidas permissões e build do Vite

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,23 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
 
 export default defineConfig({
-    // server: {
-    //     host: true, // permite acesso externo
-    //     cors: true, // habilita CORS
-    //     hmr: {
-    //         host: '10.205.203.27', // ip
-    //     },
-    // },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,  
+            refresh: true,
+            buildDirectory: 'public/build', // garante que o Laravel Vite procure aqui
         }),
     ],
+    build: {
+        outDir: 'public/build',   // saída real do Vite
+        emptyOutDir: true,        // limpa antes de buildar
+        rollupOptions: {
+            input: {
+                app: path.resolve(__dirname, 'resources/js/app.js'),
+                style: path.resolve(__dirname, 'resources/css/app.css'),
+            },
+        },
+    },
 });
